d94e8dcdb3d92637939b5d663e80898bd731034b
Cookie <mxgsoft@gmail.com>
2024-11-12 00:44:50 +0800

    Update moxogo_install.sh

--- moxogo_install.sh
+++ moxogo_install.sh
@@ -15,7 +15,7 @@
 ################################################################################
 
 OE_USER="mxg18"
-OE_HOME="/$OE_USER
+OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 # The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 # Set to true if you want to install it, false if you don't need it or have it already installed.
